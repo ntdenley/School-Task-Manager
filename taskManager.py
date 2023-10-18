5be886829e27927466a443f1c638e3f580dc7db0
Noah Denley <noah.denley@wsu.edu>
2023-10-17 18:05:18 -0700

    Added Assignment listings with due dates

--- taskManager.py
+++ taskManager.py
@@ -1,17 +1,26 @@
 import requests
 import json
 
-url = "https://wsu.instructure.com/api/v1/courses"
+url = "https://wsu.instructure.com/api/v1/users/self/enrollments"
 f = open('config.json')
 data = json.load(f)
 token = data["token"]
 
-header = {"Authorization": "Bearer "+token, "enrollment_state": "active" }
+header = {"Authorization": "Bearer "+token }
 
 r = requests.get(url, headers=header)
 
 r = r.json()   
-#r = json.dumps(r, indent=4)
+# rdump = json.dumps(r, indent=4, separators=("\n ", " = "))
 
+# print(rdump)
 for item in r:
-    print(item.get("name"))
+    course = item.get("course_id")
+    url = "https://wsu.instructure.com/api/v1/courses/"+str(course)+"/assignments"
+    r2 = requests.get(url, headers=header)
+    r2 = r2.json()
+    
+    print()
+    print(r2[0].get("course_id"))
+    for x in r2:
+        print(f'{x.get("name"):60} - {x.get("due_at")}')
